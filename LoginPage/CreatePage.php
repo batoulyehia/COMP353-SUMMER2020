7960b095635c9a730a4699ccd38b399bdbe1ddd1
Sashank <s_ubba@encs.concordia.ca>
2020-07-30 16:02:45 -0400

    Create CreatePage.php

---
+++ LoginPage/CreatePage.php
@@ -0,0 +1,101 @@
+<?php
+        $servername = "localhost";
+        $username = "root";
+        $password = "";                                             //put your mysql server password to connect [and this is password with small p]
+        $dbname = "login";                                             // whatever you do make sure you create a database named login and table named UserInfo
+        $Password = "";
+        $ErrorPass = "";
+        $Email = "";
+        $ErrorEmail = "";
+        $Valid1 = "";
+        $Valid2 = "";
+        $ErrorMessage1 = "";
+        $ErrorMessage2 = "";
+
+
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        echo "Connected successfully";                                   // use only as print to make sure connection is made successfully, to be commented out on final version
+
+        if ($_SERVER["REQUEST_METHOD"] == "POST"){
+
+          if (empty($_POST["Email"])){
+            $ErrorEmail = "Email is required.";
+          }
+          else {
+            $Email = $_POST["Email"];
+              if (!filter_var($Email, FILTER_VALIDATE_EMAIL)) {
+                $ErrorEmail = "Invalid Email format."; 
+              }
+              else{
+                $Valid1 = $Email;
+              }
+          }
+
+          if (empty($_POST["Password"])){
+            $ErrorPass = "Password is required.";
+          }
+          else{
+            $Password = $_POST["Password"];
+              if (!preg_match("/(?=^.{8,}$)(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?!.*\s)[0-9a-zA-Z!@#$%^&*(){\|}~_']*$/",$Password)){   //kept same password requirements as assignment
+                $ErrorPass = "Invalid Password format.";
+              }  
+              else {
+                $Valid2 = $Password;
+              }
+          }
+
+            if (!(isset($Valid1)) and  !(isset($Valid2))){                                                         // to enter website once login is successfull
+              $ErrorMessage1 = " Please try again."; 
+              $ErrorMessage2 = " Please try again.";
+            }
+                   
+                if (!empty($Valid1) and !empty($Valid2)){
+                 $sql = "INSERT INTO UserInfo (Email, Password)
+                  VALUES ('$Valid1','$Valid2')";              //php code to be added from account creation html page
+
+                  if ($conn->query($sql) === TRUE) {
+                      echo "New record created successfully";            // use only as print to make sure connection is made successfully, to be commented out on final version
+                  } else {
+                      echo "Error: " . $sql . "<br>" . $conn->error;    // use only as print to make sure connection is made successfully, to be commented out on final version
+                  }
+                  header("Location: LoginPage.php");
+                }
+
+        }
+?>
+
+<!DOCTYPE html>
+  <html lang = "en">
+      <head>
+        <meta charset="utf-8"> 
+        <title>Login</title>
+        <link rel="stylesheet" href="loginStyle.css">
+        <style>
+        .error {color: #FF0000;}
+        </style>
+      </head>
+      <body >
+        <div class="login-page">
+          <div class="form">
+            <form class="login-form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+              <span class="error"><?php echo $ErrorEmail, $ErrorMessage1;?></span>
+              <input type="Email" placeholder="Email" name="Email" />
+              <span class="error"><?php echo $ErrorPass, $ErrorMessage2;?></span>
+              <input type="Password" placeholder="Password" name="Password" />
+              <input class="button" type="submit" name="submit" value="Create" />                         
+
+
+                <?php
+                  $conn->close();                                                                               //at the end of program end connection to mysql database server
+                ?>
+            </form>
+          </div>
+        </div>
+      </body>
+  </html>
