546aac8e2db06b7e4656c602a43c830933efe31d
Sashank <s_ubba@encs.concordia.ca>
2020-08-04 03:57:14 -0400

    Update DDL.sql

--- DDL.sql
+++ DDL.sql
@@ -1,34 +1,33 @@
-CREATE TABLE user_account (
-    user_ID integer AUTO_INCREMENT,
-    email varchar(100),
-    sa_ID integer,
-    password varchar(100),
-    first_name varchar(100),
-    last_name varchar(100),
-    status varchar(100) DEFAULT 'active',
-    balance real DEFAULT 0,
-    PRIMARY KEY (user_ID, email),
-    FOREIGN KEY (sa_ID) REFERENCES suffering_account(sa_ID)
-);
 CREATE TABLE suffering_account (
-    sa_ID integer,
-    sa_status varchar(100),
-    months integer,
-    PRIMARY KEY (sa_ID)
+sa_ID integer,
+sa_status varchar(100) DEFAULT 'non-suffering',
+months integer DEFAULT 0,
+PRIMARY KEY (sa_ID)
 );
+
+CREATE TABLE user_account (
+user_ID integer AUTO_INCREMENT,
+email varchar(100),
+sa_ID integer,
+password varchar(100),
+first_name varchar(100),
+last_name varchar(100),
+status varchar(100) DEFAULT 'active',
+balance real DEFAULT 0,
+PRIMARY KEY (user_ID),
+FOREIGN KEY (sa_ID) REFERENCES suffering_account(sa_ID)
+);
+
 CREATE TABLE employee (
-    user_ID integer,
-    email varchar(100),
-    employee_membership_type varchar(100) DEFAULT 'basic',
-    PRIMARY KEY (user_ID, email),
-    FOREIGN KEY (user_ID) REFERENCES user_account(user_ID),
-    FOREIGN KEY (email) REFERENCES user_account(email)
+user_ID integer,
+employee_membership_type varchar(100) DEFAULT 'basic',
+PRIMARY KEY (user_ID),
+FOREIGN KEY (user_ID) REFERENCES user_account(user_ID)
 );
+
 CREATE TABLE employer (
-    user_ID integer,
-    email varchar(100),
-    employer_membership_type varchar(100) DEFAULT 'prime',
-    PRIMARY KEY (user_ID, email),
-    FOREIGN KEY (user_ID) REFERENCES user_account(user_ID),
-    FOREIGN KEY (email) REFERENCES user_account(email)
+user_ID integer,
+employer_membership_type varchar(100) DEFAULT 'prime',
+PRIMARY KEY (user_ID),
+FOREIGN KEY (user_ID) REFERENCES user_account(user_ID)
 );
