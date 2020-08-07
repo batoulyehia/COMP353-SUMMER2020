e62c3fac9cb7b64bb58c00bcaf0e0075953e4b5e
Ramy Elhoufy <pandavader55@gmail.com>
2020-08-06 21:18:56 -0400

    Add files via upload
    populated the apply table and filled out the missing queries

--- Queries.sql
+++ Queries.sql
@@ -46,11 +46,19 @@ iv.
 		insert into give_offer (employee_user_ID, employer_user_ID, job_ID) values (181, 191, 9999);
         
 v.
-	waiting for the apply relation table
+	/*report of a posted job by an employer*/
+		SELECT *
+		FROM job J, apply A
+		WHERE J.user_ID = 191 AND A.job_ID = J.job_ID
     
 vi. 
-	waiting for the apply relation table
-
+	/*report of posted jobs by an employer during a specific period of time*/
+		SELECT *, COUNT(A.job_ID)
+		FROM job J, apply A
+		WHERE J.user_ID = 191 AND J.date_posted >= '1980-01-05' AND J.date_posted <= '2040-06-02'  AND A.job_ID = J.job_ID
+        GROUP BY A.job_ID
+        HAVING app_status = 'accepted'
+        
 vii.
 	/*create an employee*/ /*to create an employer or an employee, we have to create a user_account first*/
 		INSERT INTO user_account (user_ID,sa_ID,password,first_name,last_name,balance,email) values(8888,NULL,'testing','Ramy','Elhoufy',0,'r.elhoufy@gmail.com');
@@ -79,23 +87,34 @@ viii.
 		WHERE job_title = "Civil Engineer"
         
 ix.
-	waiting for the apply relation table
+	/*apply for a job by an employee*/
+		INSERT INTO apply(employee_user_ID, job_ID, app_status, date_applied) values(181, 9999, 'applied', '2020/08/06');
 
 x.
-	waiting for the apply relation table
+	/*accept/deny an offer by an employee*/
+		UPDATE apply
+		SET app_status = 'rejected'
+		WHERE employee_user_ID = 181 AND job_ID = 9999
 
 xi.
-	waiting for the apply relation table
+	/*widthraw from an applied job by an employee*/
+		UPDATE apply
+		SET app_status = 'widthrawed'
+		WHERE employee_user_ID = 8000 AND job_ID = 97
 
 xii.
-		/*delete a profile*/ /*have to delete from employee and user_account*/
+	/*delete a profile*/ /*have to delete from employee and user_account*/
 		DELETE FROM employee
 		WHERE user_ID = some_ID
 		DELETE FROM user_account
 		WHERE user_ID = some_ID
 
 xiii.
-	waiting for the apply relation table
+	/*report of applied jobs by an employee during a specific period of time*/
+		SELECT *
+		FROM job J, apply A
+		WHERE A.date_applied >= '1980-01-05' AND A.date_applied <= '2040-06-02'  AND A.employee_user_ID = 181 AND A.job_ID = J.job_ID
+        
     
 xiv.
 	/*add method of payment*/
