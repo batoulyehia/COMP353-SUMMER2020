3ce67bed2f9b7361d170f35fe8e97de49ccf6c75
Sashank Subba <s_ubba@encs.concordia.ca>
2020-08-09 02:37:58 -0400

    [admin_activation] Admins can successfully activate/deactivate user accounts.

--- admin/user-activation.php
+++ admin/user-activation.php
@@ -25,23 +25,27 @@
             }
 
             //summary of users
-            $totalEmployees_query = $conn->prepare("SELECT COUNT(*) FROM employee e");
-            $totalEmployees_query->execute();
-            $totalEmployees = $totalEmployees_query->fetch(\PDO::FETCH_ASSOC);
+            $totalActive_query = $conn->prepare("SELECT COUNT(*) FROM user_account acc WHERE acc.status='active'");
+            $totalActive_query->execute();
+            $totalActive = $totalActive_query->fetch(\PDO::FETCH_ASSOC);
 
-            $totalEmployers_query = $conn->prepare("SELECT COUNT(*) FROM employer e");
-            $totalEmployers_query->execute();
-            $totalEmployers = $totalEmployers_query->fetch(\PDO::FETCH_ASSOC);
+            $totalInactive_query = $conn->prepare("SELECT COUNT(*) FROM user_account acc WHERE acc.status='inactive'");
+            $totalInactive_query->execute();
+            $totalInactive = $totalInactive_query->fetch(\PDO::FETCH_ASSOC);
 
-            //employee info
-            $employeeInformation_query = $conn->prepare("SELECT first_name, last_name, E.user_ID ,email, E.employee_membership_type , AC.status FROM user_account AC, employee E WHERE AC.user_ID = E.user_ID");
-            $employeeInformation_query->execute();
-            $employeeInformation = $employeeInformation_query->fetchAll(PDO::FETCH_ASSOC);
+            //active users info
+            $ActiveInformation_query = $conn->prepare("SELECT AC.first_name, AC.last_name, AC.user_ID , AC.email, AC.balance , AC.status FROM user_account AC WHERE AC.status = 'active' 
+                                                       UNION
+                                                       SELECT AC.first_name, AC.last_name, AC.user_ID , AC.email, AC.balance , AC.status FROM user_account AC WHERE AC.status = 'active'");
+            $ActiveInformation_query->execute();
+            $ActiveInformation = $ActiveInformation_query->fetchAll(PDO::FETCH_ASSOC);
 
-            //employer info
-            $employerInformation_query = $conn->prepare("SELECT first_name, last_name, E.user_ID ,email, E.employer_membership_type, AC.status FROM user_account AC, employer E WHERE AC.user_ID = E.user_ID");
-            $employerInformation_query->execute();
-            $employerInformation = $employerInformation_query->fetchAll(PDO::FETCH_ASSOC);
+            //deactivated users info 
+            $InactiveInformation_query = $conn->prepare("SELECT AC.first_name, AC.last_name, AC.user_ID , AC.email, AC.balance , AC.status FROM user_account AC WHERE AC.status = 'inactive' 
+                                                         UNION
+                                                         SELECT AC.first_name, AC.last_name, AC.user_ID , AC.email, AC.balance , AC.status FROM user_account AC WHERE AC.status = 'inactive'");
+            $InactiveInformation_query->execute();
+            $InactiveInformation = $InactiveInformation_query->fetchAll(PDO::FETCH_ASSOC);
 
         ?>
         <nav class="navbar navbar-expand-lg navbar-light bg-light">
@@ -60,63 +64,106 @@
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Total Number of Employers</th>
-                        <th scope="col">Total Number of Employees</th>
+                        <th scope="col">Total Number of Active Users</th>
+                        <th scope="col">Total Number of Deactivated Users</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?php echo $totalEmployees['COUNT(*)'] ?></td>
-                        <td><?php echo $totalEmployees['COUNT(*)'] ?></td>
+                        <td><?php echo $totalActive['COUNT(*)'] ?></td>
+                        <td><?php echo $totalInactive['COUNT(*)'] ?></td>
                     </tr>
                 </tbody>
             </table>
-
-            <!--dropdown here-->
-            <h5 style="margin-top: 40px;">List of Employees</h5>
+            
+            <h5 style="margin-top: 40px;">List of Active Users</h5>
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Employee Name</th>
-                        <th scope="col">Employee User ID</th>
+                        <th scope="col">User ID</th>
+                        <th scope="col">User Name</th>
                         <th scope="col">Email</th>
-                        <th scope="col">Membership Type</th>
+                        <th scope="col">Balance</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <?php foreach($employeeInformation as $employeeInformation){ ?>
-                            <td><?php echo $employeeInformation['first_name'],' ', $employeeInformation['last_name'] ?></td>
-                            <td><?php echo $employeeInformation['user_ID']?></td>
-                            <td><?php echo $employeeInformation['email']?></td>
-                            <td><?php echo $employeeInformation['employee_membership_type']?></td>
-                            <td><?php echo $employeeInformation['status']?></td>
+                        <?php foreach($ActiveInformation as $ActiveInformation){ ?>
+                            <td><?php echo $ActiveInformation['user_ID']?></td>
+                            <td><?php echo $ActiveInformation['first_name'],' ', $ActiveInformation['last_name'] ?></td>
+                            <td><?php echo $ActiveInformation['email']?></td>
+                            <td><?php echo $ActiveInformation['balance'] . "$"?></td>
+                            <td><?php echo $ActiveInformation['status']?></td>
+                            <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+                            <td>Update Status:
+                                    <select name="status">                               <!--//echo "status" . $ActiveInformation['user_ID']-->
+                                    <option value="active" selected>active</option>
+                                    <option value="inactive">inactive</option>
+                                </select>
+                            </td>
+                            <td>
+                                <div style="display:flex; justify-content: center;">
+                                    <button type="submit" name="<?php echo "status" . $ActiveInformation['user_ID'] ?>" class="btn btn-success btn-lg" style="font-size:16px;">Update</button>
+                                </div>
+                            </td>
+                            </form>
+                            <?php
+                                $ID = $ActiveInformation['user_ID'];
+                                if(isset($_POST["status" . $ActiveInformation['user_ID']])){                //if(isset($_POST["status" . $ActiveInformation['user_ID']])){
+                                    if(!($ActiveInformation['status'] == $_POST["status"])) {              //$_POST["status" . $ActiveInformation['user_ID']]
+
+                                        $update = "UPDATE user_account
+                                                   SET status = 'inactive'
+                                                   WHERE user_ID = $ID";
+                                        try {
+                                        $conn->exec($update);
+                                        echo("<meta http-equiv='refresh' content='0.1'>");
+                                        } catch(PDOException $e) {
+                                        echo $sql . "<br>" . $e->getMessage();
+                                        }
+                                    } else {
+                                    echo 'SAME ANSWERRRRRRRRRRRRRRRR';
+                                    }
+                                }
+                            ?>
                     </tr>
                     <?php } ?>
                 </tbody>
             </table>
-            
-            <!--dropdown here-->
-            <h5 style="margin-top: 40px;">List of Employers</h5>
+                
+            <h5 style="margin-top: 40px;">List of Deactivated Users</h5>
             <table class="table">
                 <thead>
                     <tr>
-                        <th scope="col">Employer Name</th>
-                        <th scope="col">Employer User ID</th>
+                        <th scope="col">User ID</th>
+                        <th scope="col">User Name</th>
                         <th scope="col">Email</th>
-                        <th scope="col">Membership Type</th>
+                        <th scope="col">Balance</th>
                         <th scope="col">Status</th>
+                        <th scope="col">Action</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <?php foreach($employerInformation as $employerInformation){ ?>
-                            <td><?php echo $employerInformation['first_name'],' ', $employerInformation['last_name'] ?></td>
-                            <td><?php echo $employerInformation['user_ID']?></td>
-                            <td><?php echo $employerInformation['email']?></td>
-                            <td><?php echo $employerInformation['employer_membership_type']?></td>
-                            <td><?php echo $employerInformation['status']?></td>
+                        <?php foreach($InactiveInformation as $InactiveInformation){ ?>
+                            <td><?php echo $InactiveInformation['user_ID']?></td>
+                            <td><?php echo $InactiveInformation['first_name'],' ', $InactiveInformation['last_name'] ?></td>
+                            <td><?php echo $InactiveInformation['email']?></td>
+                            <td><?php echo $InactiveInformation['balance'] . "$"?></td>
+                            <td><?php echo $InactiveInformation['status']?></td>
+                            <td>Update Status:
+                                <select name="<?php echo "status" . $ActiveInformation['user_ID'] ?>">
+                                    <option value="active">active</option>
+                                    <option value="inactive" selected>inactive</option>
+                                </select>
+                            </td>
+                            <td>
+                                <div style="display:flex; justify-content: center;">
+                                    <button type="button" class="btn btn-success btn-lg" style="font-size:16px;">Update</button>
+                                </div>
+                            </td>
                     </tr>
                     <?php } ?>
                 </tbody>
